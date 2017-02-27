121e7f875194bc019d12f756348a9420dc8b5d21
Dennis Fiedler <dennis.fiedler@uniprank.com>
2017-02-27 01:28:42 +0100

    put bundle for system.config.js (#2) && example update

---
+++ vendor.ts
@@ -0,0 +1,12 @@
+// Angular
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+
+// RxJS
+import 'rxjs';
+
+// Zone
+import 'zone.js/dist/zone';
